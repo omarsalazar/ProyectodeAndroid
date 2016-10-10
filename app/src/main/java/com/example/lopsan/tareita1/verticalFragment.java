ebcb278a0eaca056092e0c0c6f34d0576e8c2471
omarazalazar <salazarpazomar@gmail.com>
2016-10-10 02:20:30 -0500

    Se agrego un spinner pero no le entendi y fallo xD

--- app/src/main/java/com/example/lopsan/tareita1/verticalFragment.java
+++ app/src/main/java/com/example/lopsan/tareita1/verticalFragment.java
@@ -7,16 +7,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Button;
 import android.support.v7.widget.AppCompatButton;
 import android.view.View.OnClickListener;
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.app.Activity;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import java.util.Arrays;
 
 
-
-public class verticalFragment extends Fragment {
+public class verticalFragment extends Fragment implements OnItemSelectedListener{
 
     EditText nombre;
     EditText direccion;
@@ -26,6 +37,10 @@ public class verticalFragment extends Fragment {
     EditText contrasena2;
     Button clear;
     Button enviar;
+
+    TextView selected;
+    Spinner spinner;
+    String[] users;
 
     public verticalFragment() {
         // Required empty public constructor
@@ -45,6 +60,21 @@ public class verticalFragment extends Fragment {
         contrasena2 = (EditText) view.findViewById(R.id.edit_confirm);
         clear = (Button) view.findViewById(R.id.no_se);
         enviar = (Button) view.findViewById(R.id.button);
+        spinner = (Spinner) view.findViewById(R.id.spinner);
+        selected = (TextView)view.findViewById(R.id.selected);
+
+        users = getResources().getStringArray(R.array.user);
+
+        ArrayAdapter<String> adapter;
+
+        adapter = new ArrayAdapter<String>(verticalFragment.this.getActivity(), android.R.layout.simple_spinner_item, users);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+
+
         clear.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,6 +103,46 @@ public class verticalFragment extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //MenuInflater inflater = getActivity().getMenuInflater();
+        //super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position,
+                               long id) {
+        String user;
+        user = (String) parent.getItemAtPosition(position);
+        selected.setText(
+                this.getResources().getString(R.string.selected) + " " + Arrays.toString(users)
+        );
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
+
+    }
+
+
+
     protected void lanzarActividad(String name, String adres, String phone, String email, String pass) {
         Intent intent;
         intent  = new Intent(verticalFragment.this.getActivity(), Main2Activity.class);
@@ -83,4 +153,6 @@ public class verticalFragment extends Fragment {
         intent.putExtra("contrasena", pass);
         this.startActivity(intent);
     }
+
+
 }
